2334157362aac578161908af1147991548b91d3c
Jason Sobreiro <jason.sobreiro@up.edu.br>
2021-09-16 21:57:05 -0300

    Primeira versão do projeto com recycler view

---
+++ Noite/Quinta/Aula07/app/src/main/java/com/example/aula07/activities/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.aula07.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.aula07.R;
+import com.example.aula07.adapter.SerieAdapter;
+import com.example.aula07.controller.SerieDAO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvSeries;
+    FloatingActionButton fabAdd;
+
+    SerieDAO serieDAO;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvSeries = findViewById(R.id.rvSeries);
+        fabAdd = findViewById(R.id.fabAdd);
+
+        serieDAO = new SerieDAO();
+
+        fabAdd.setOnClickListener(v -> {
+            startActivity(new Intent(this, CadastroActivity.class));
+        });
+
+        // criar um layout manager para setar na recycler view
+        RecyclerView.LayoutManager meuLayout = new LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL,
+                false
+        );
+
+        // setar o layout criado para a recycler view
+        rvSeries.setLayoutManager(meuLayout);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // criar o adaptador customizado baseado na nossa calsse SerieAdapter
+        SerieAdapter meuAdapter = new SerieAdapter(this, serieDAO);
+
+        // setar o adaptador criado para a recycler view
+        rvSeries.setAdapter(meuAdapter);
+    }
+}
---
+++ Noite/Quinta/Aula07/app/src/main/java/com/example/aula07/adapter/SerieViewHolder.java
@@ -0,0 +1,29 @@
+package com.example.aula07.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.aula07.R;
+
+
+public class SerieViewHolder extends RecyclerView.ViewHolder {
+
+    // criar atributos da interface
+    TextView tvTitulo;
+    TextView tvGenero;
+    TextView tvTemporadas;
+    TextView tvOndeAssistir;
+
+    public SerieViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        // referenciar os elementos de interface
+        tvTitulo       = itemView.findViewById(R.id.tvTitulo);
+        tvGenero       = itemView.findViewById(R.id.tvGenero);
+        tvTemporadas   = itemView.findViewById(R.id.tvTemporadas);
+        tvOndeAssistir = itemView.findViewById(R.id.tvOndeAssistir);
+    }
+}
